443273a5b5645108c6db15fabc43f8904efe64c2
Adhi-prog <adithyaradhakrishanan2022@gmail.com>
2024-09-17 14:12:33 +0530

    updated

--- react/src/NavBar.jsx
+++ react/src/NavBar.jsx
@@ -10,4 +10,22 @@ const Navbar = ({ data }) => {
   }
   
   export default Navbar;
+// import React from 'react';
+
+// const Navbar = () => {
+//   return (
+//     <nav className="bg-gray-800 p-4 flex justify-between items-center">
+//       <div className="text-white text-lg">My Portfolio</div>
+//       <div className="flex space-x-4">
+//         <a href="#about" className="text-white">About</a>
+//         <a href="#projects" className="text-white">Projects</a>
+//         <a href="#skills" className="text-white">Skills</a>
+//         <a href="#contact" className="text-white">Contact</a>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
   
